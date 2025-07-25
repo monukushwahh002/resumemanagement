c8e07a157a80b9870c067d580a54092f1ba4e90e
monukushwahh002 <mkh7018576@gmail.com>
2025-07-26 00:25:27 +0530

    rout are changed

--- server.js
+++ server.js
@@ -46,7 +46,7 @@ app.get("/", (req, res) => {
 });
 
 app.get("/leads", (req, res) => {
-  res.sendFile(path.join(__dirname, "views", "Leads.HTML"));
+  res.sendFile(path.join(__dirname, "views", "leads.html"));
 });
 
 // Resume upload setup
